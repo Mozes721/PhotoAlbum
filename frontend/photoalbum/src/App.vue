bf432fcc94b0eeecfe8ece09566b31d276d0a74f
Mozes721 <Mozesthegreat@yahoo.com>
2022-04-17 14:41:19 +0200

    added add button

--- frontend/photoalbum/src/App.vue
+++ frontend/photoalbum/src/App.vue
@@ -5,11 +5,11 @@
           <span class="text-white font-semibold tracking-tight text-3xl font-bold p-3">PhotoAlbum</span>
         </div>
     </nav>
-    <div class="flex min-h-screen">
-      <div class="w-1/7 bg-gray-50 border-r border-gray-200">
+    <div class="grid grid-cols-5 gap-2 min-h-screen  divide-gray-400">
+      <div class="bg-gray-50 border-r border-gray-200">
           <Albums @changeAlbum="updateAlbum" />
       </div>
-      <div class="w-6/7 bg-gray-50 border-r border-gray-200">
+      <div class="col-span-4 bg-gray-50 border-r border-gray-200">
           <Pictures :album="this.selectedAlbum"/>
       </div>
     </div>
--- frontend/photoalbum/src/components/Albums.vue
+++ frontend/photoalbum/src/components/Albums.vue
@@ -1,13 +1,26 @@
 <template>
+  <main>
       <nav class="mt-10 px-6 ">
               <a class="hover:text-gray-800 hover:bg-gray-100 flex items-center p-2 my-6 transition-colors dark:hover:text-white dark:hover:bg-gray-600 duration-200  text-gray-600 dark:text-gray-400 rounded-lg" v-for="(album) in this.albums" :key="album.id">
+                <div class="grid">
                 <span class="mx-4 text-lg font-normal cursor-pointer" @click="albumSelect(album)">
                         {{album.name}}
                     </span>
-                    <span class="flex-grow text-right">
+                    <span class="mx-4 text-lg font-normal cursor-pointer">
+                      <a href="#" class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:ring-red-300 dark:bg-red-600 dark:hover:bg-blue-red dark:focus:ring-blue-red">
+                          Delete
+                          <img class="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" src='../../assets/delete.png' />
+                      </a>
                     </span>
+                </div>
+                </a>
+                <a href="#" class="flex h-1-4 justify-center items-center w-1-3 ">
+                <div class="items-center ">
+                <img class="ml-2 -mr-1 w-8 h-8 hover:bg-green-400" fill="currentColor" viewBox="0 0 20 20" src='../../assets/plus.png' />
+                </div>
                 </a>
             </nav>
+  </main>
 </template>
 
 <style>
@@ -29,13 +42,15 @@ export default {
   },
   async created() {
 	  try {
-		const response = await axios.get('http://localhost:8000/api/albums')
-		this.albums = response.json()
-	  }catch (e) 
-	  {
-		this.error.push(e)
-	  }
-
+		    const response = await fetch('http://localhost:8000/api/albums/')
+    if (!response.ok) {
+      throw new Error(`Error! status: ${response.status}`);
+    }
+        this.albums = await response.json()
+    }
+    catch (err) {
+        console.log(err);
+    }
   },
   methods: {
     albumSelect(album) {
