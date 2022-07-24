d4e424dd84493eb085ebf639e006ee0d6bdb7d54
r.taujenis <r.taujenis@samsung.com>
2022-07-24 20:45:04 +0200

    Delete method works on albums and pictures

--- backend/project/project/router.py
+++ backend/project/project/router.py
@@ -4,6 +4,5 @@ from rest_framework import routers
 router = routers.DefaultRouter()
 router.register('albums', AlbumViewSet)
 router.register('photos', PhotoViewSet)
-# router.register('albums/delete', AlbumViewSet.deleteObjects)
-# router.register('photos/delete', PhotoViewSet.deleteObjects)
+
 
